7d5844e5877c07e9e931a2cd4bc97ecdcb4713a7
Shashank Chaurasia <xhaxhank46@gmail.com>
2025-02-06 20:01:11 +0530

    Bug Fixes

--- routes/generalRoutes.js
+++ routes/generalRoutes.js
@@ -4,7 +4,21 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
     try{
-        res.render('home');
+        const alertMessage = `
+            In order to test the functionality of the website, please use the credentials below.<br>
+            Request you to first sign in as a user, place an order, and then login as an admin to accept the order and other steps.<br>
+            You can also sign-up as an user, no need for actual email. (Work in progress to verify email and extra functionality)<br>
+            Thank you for checking out my work.<br>
+            <br>
+            <strong>Admin Credentials:</strong><br>
+            Email: admin@admin.com<br>
+            Password: admin@123<br>
+            <br>
+            <strong>User Credentials:</strong><br>
+            Email: test@test.com<br>
+            Password: test@123<br>
+    `;
+        res.render('home',{alertMessage});
     } catch(err) {
         next(err);
     }
